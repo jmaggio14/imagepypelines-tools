c5671ea35ee53494119e6a9353bd3759e9f1d199
Jeff Maggio <jmaggio14@gmail.com>
2019-02-25 18:59:03 -0500

    adds setup for this repo

---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+current_dir = os.path.dirname(__file__)
+
+# load __version__, __author__, __email__, etc variables
+with open(os.path.join(current_dir,'version_info.py')) as f:
+    exec(f.read())
+
+long_description = ''
+if os.path.exists(os.path.join(current_dir,'README.md')):
+    with open(os.path.join(current_dir,'README.md'), 'r') as f:
+        long_description = f.read()
+
+setup(name='imagepypelines-tools',
+      version=__version__,
+      description=__description__,
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author=__author__,
+      author_email=__email__,
+      license=__license__,
+      url=__url__,
+      download_url=__download_url__,
+      maintainer=__maintainer__,
+      maintainer_email=__maintainer_email__,
+      keywords=__keywords__,
+      python_requires=__python_requires__,
+      platforms=__platforms__,
+      classifiers=__classifiers__,
+      packages=find_packages(),
+      include_package_data=True,
+      install_requires=requirements,
+      scripts=['scripts/xmlproc_parse', 'scripts/xmlproc_val']
+      )
---
+++ version_info.py
@@ -0,0 +1,65 @@
+# @Email: jmaggio14@gmail.com
+# @Website: https://www.imagepypelines.org/
+# @License: https://github.com/jmaggio14/imagepypelines/blob/master/LICENSE
+# @github: https://github.com/jmaggio14/imagepypelines
+#
+# Copyright (c) 2018 Jeff Maggio, Nathan Dileas, Ryan Hartzell
+__all__ = [
+        '__name__'
+        '__version__'
+        '__description__'
+        '__author__'
+        '__email__'
+        '__license__'
+        '__url__'
+        '__download_url__'
+        '__maintainer__'
+        '__maintainer_email__'
+        '__keywords__'
+        '__python_requires__'
+        '__platforms__'
+        '__classifiers__'
+        '__credits__'
+        '__status__'
+        '__copyright__'
+            ]
+
+
+__name__ = 'imagepypelines-tools'
+__version__ = '0.1.0-alpha'
+__description__ = 'accessory library to augment the image processing library imagepypelines'
+__author__ = 'Jeff Maggio, Nathan Dileas, Ryan Hartzell'
+__email__ = "jmaggio14@gmail.com"
+__license__ = "MIT"
+__url__ = "https://www.imagepypelines.org"
+__download_url__ = 'https://github.com/jmaggio14/imagepypelines'
+__maintainer__ = "Jeff Maggio"
+__maintainer_email__ = "jmaggio14@gmail.com"
+__keywords__ = 'imaging-science machine-learning computer-vision data-pipelines docker'
+__python_requires__ = '>=3.4.*,!=3.7.*'
+__platforms__ = ["Windows", "Linux", "Mac OS-X", "Unix"]
+__classifiers__ = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Science/Research',
+    # 'Topic :: Software Development',
+    # 'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Image Recognition',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    # 'Programming Language :: Python :: 3.7', # JM: 3.7 must wait for tensorflow
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Natural Language :: English',
+    ]
+
+__credits__ = ["Jeff Maggio", "Nathan Dileas", "Ryan Hartzell"]
+__status__ = "Experimental"
+__copyright__ = "Copyright 2018 (c) " + __author__
