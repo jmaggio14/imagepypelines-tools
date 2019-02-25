0a4dccdda6d156e1f329962128ef340f56bb80d4
Jeff Maggio <jmaggio14@gmail.com>
2019-02-25 17:29:31 -0500

    updates devel to work with windows

--- devel.py
+++ devel.py
@@ -6,27 +6,26 @@ import subprocess
 
 HOME = os.path.expanduser('~')
 CURRENT_DIR = os.path.abspath( os.path.dirname(__file__) )
+POSIX_PATH = CURRENT_DIR.replace(HOME,'/root').replace(os.sep,'/')
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--image',default='devel')
+parser.add_argument('--image',default='imagepypelines')
 parser.add_argument('--display',default=':0')
-parser.add_argument('--xauth',default='/tmp/.docker.xauth')
 
 args = parser.parse_args()
 
+# subprocess.run('touch {}'.format(args.xauth), shell=True)
+# subprocess.run('xauth nlist {} | sed -e \'s/^..../ffff/\' | xauth -f {} nmerge -'.format(args.display, args.xauth),
+#                shell=True)
 
-subprocess.run('touch {}'.format(args.xauth), shell=True)
-subprocess.run('xauth nlist {} | sed -e \'s/^..../ffff/\' | xauth -f {} nmerge -'.format(args.display, args.xauth),
-               shell=True)
-
-CMD = ['nvidia-docker',
+CMD = ['docker',
          'run',
          # make interactive
          '-it',
          # mount the current directory
-         '-v', '{0}:{0}'.format(CURRENT_DIR),
-         '-w', '{0}'.format(CURRENT_DIR),
+         '-v', '{0}:{1}'.format(CURRENT_DIR,POSIX_PATH),
+         '-w', '{0}'.format(POSIX_PATH),
          # set (and remove) limits on hardware resources
         '--security-opt=seccomp:unconfined',
         '--privileged',
@@ -34,20 +33,13 @@ CMD = ['nvidia-docker',
         '--ulimit', 'rtprio=99:99',
         '--ulimit', 'nice=-20:-20',
         # clion setup
-        '-v', '{0}/.CLion2018.3:{0}/.CLion2018.3'.format(HOME),
-        '-v', '{0}/Documents/installed/clion-2018.3.4:{0}/Documents/installed/clion-2018.3.4'.format(HOME),
-        '-v', '{0}/.java:{0}/.java'.format(HOME),
-        '-v', '{0}/.java:{0}/.java'.format(HOME),
-        '-v', '{0}/.config:{0}/.config'.format(HOME),
-        # graphics and device accesss
-        '-v', '/tmp/.X11-unix:/tmp/.X11-unix',
-        '-v', '/run/dbus:/run/dbus',
-        '-v', '/dev:/dev',
-        '-e', 'DISPLAY=:0',
+        '-e', 'DISPLAY={0}'.format(args.display),
         '-e', 'QT_X11_NO_MITSHM=1',
-        '-e', 'XAUTHORITY=/tmp/.docker.xauth',
+        # '-e', 'XAUTHORITY=/tmp/.docker.xauth',
         '-e', 'HOST_HOME={0}'.format(HOME),
+        '-e','force_color_prompt=1',
         args.image
         ]
 
-subprocess.run(CMD)
+subprocess.call(CMD)
+# subprocess.run(CMD)
