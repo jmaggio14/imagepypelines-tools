4a7bd0f699f6ae0dcf7adaf66a4663337a4550c7
Jeff Maggio <jmaggio14@gmail.com>
2019-03-09 13:08:36 -0500

    adds IP logo printout on startup -Ryan

--- dockerfiles/imagepypelines-base/startup.sh
+++ dockerfiles/imagepypelines-base/startup.sh
@@ -1,4 +1,37 @@
 echo -e "
+################################################################################
+##############################Ndyo/:.        .-/+shN############################
+##########################my/.                       :sd########################
+#######################mo-                               /h#####################
+#####################h\033[38;5;208mo/+o+/.\e[39m\e[49m                              .oN##################
+###################s\033[38;5;208m-oooooooo/\e[39m\e[49m                                +N################
+#################d. \033[38;5;208m:ooooooooo \e[39m\e[49m                                o################
+################+   \033[38;5;208m.oooooooo+ \e[39m\e[49m                                  -m#############
+##############N-     \033[38;5;208m./oooo+:\e[39m\e[49m                                      h############
+#############N.     \033[38;5;208m.---::--.           .:::::::-../////////::.\e[39m\e[49m     h###########
+#############-      \033[38;5;208m+ooooooooo.         :oooooooooossssssssssss+\e[39m\e[49m     m##########
+############o       \033[38;5;208m+ooooooooo.         :ooooooooossssssssssssss+\e[39m\e[49m    -##########
+###########N        \033[38;5;208m+ooooooooo.         :ooooooooossssssssssssss+\e[39m\e[49m     y#########
+###########s        \033[38;5;208m+ooooooooo.         :ooooooooossssssssssssss+\e[39m\e[49m     -#########
+###########:        \033[38;5;208m+ooooooooo.         :ooooooooo:    :sssssssss+\e[39m\e[49m     N########
+###########.        \033[38;5;208m+ooooooooo.         :ooooooooo:   -sssssssss+\e[39m\e[49m      d########
+###########.        \033[38;5;208m+ooooooooo.         :ooooooooo:   -sssssssss+\e[39m\e[49m      d########
+###########:        \033[38;5;208m+ooooooooo.         :ooooooooo:   -sssssssss+\e[39m\e[49m      N########
+###########s        \033[38;5;208m+ooooooooo.         :ooooooooo:   -sssssssss+\e[39m\e[49m     -#########
+###########N        \033[38;5;208m/ooooooooo:---------/ooooooooo/---/sssssssss+\e[39m\e[49m     y#########
+############o        \033[38;5;208m./ooooooossssssss/ :ossssssssssssssssssssss+\e[39m\e[49m    .##########
+#############-          \033[38;5;208m:ooooossssso:   :ooossssssssssssssssssss+\e[39m\e[49m    d##########
+#############N.           \033[38;5;208m-+oosss+-     :oooooosssssssssssssssss/\e[39m\e[49m   h###########
+##############N-            \033[38;5;208m./s/.       :oooooooossssssssssssso:\e[39m\e[49m   h############
+################+                       \033[38;5;208m:ooooooooo: \e[39m\e[49m             -m#############
+#################h.                     \033[38;5;208m:ooooooooo:\e[39m\e[49m             o###############
+###################s.                   \033[38;5;208m:ooooooooo:\e[39m\e[49m           +N################
+#####################y-                 \033[38;5;208m:ooooooooo:\e[39m\e[49m         oN##################
+#######################mo-              \033[38;5;208m:ooooooooo-\e[39m\e[49m      /h#####################
+##########################my/.          \033[38;5;208m:oooooooo/\e[39m\e[49m   :od########################
+##############################Ndyo/:. -///+ossoshm##############################
+################################################################################"
+echo -e "
 Welcome to the imagepypelines virtual container! This docker image contains all dependencies you need to run vanilla imagepypelines apps. The source for this dockerfile can be found here: https://github.com/jmaggio14/imagepypelines-tools
 
 ENV VARIABLES:
