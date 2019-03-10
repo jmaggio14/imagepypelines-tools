f3919262de755ddaa0a35b23d51756d4e2449032
Jeff Maggio <jmaggio14@gmail.com>
2019-03-10 15:19:05 -0400

    updates version

--- version_info.py
+++ version_info.py
@@ -26,7 +26,7 @@ __all__ = [
 
 
 __name__ = 'imagepypelines-tools'
-__version__ = '0.2.2-alpha'
+__version__ = '0.2.3-alpha'
 __description__ = 'accessory library to augment the image processing library imagepypelines'
 __author__ = 'Jeff Maggio, Nathan Dileas, Ryan Hartzell'
 __email__ = "jmaggio14@gmail.com"
