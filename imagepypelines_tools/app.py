75ee4c1379141759ab455965abff61c0f129f76c
unknown <rah3156@rit.edu>
2019-04-10 02:16:08 -0400

    Functional app.py Flask app with bootswatch css overlay... AND PROG BARS

---
+++ imagepypelines_tools/app.py
@@ -0,0 +1,27 @@
+"""
+testing script for now, but this should probably end up as the driver for our app
+~~~~~~~~~~~~
+
+Things I'm going to do here:
+
+1) Get a basic index.html displayed with correct bootstrap css theme
+
+2) Figure out how to do form or radio button input client side and print it server side (or use it to update something css-y from server to client)
+
+3) Display an image or array or graph using matplotlib and numpy
+"""
+
+from flask import Flask, flash, redirect, render_template, request, session, abort
+
+import os
+
+app = Flask(__name__, template_folder='site', static_folder='site')
+app.debug = True
+# print(app.root_path)
+
+@app.route("/")
+def welcome():
+    return render_template("templates/index.html")
+
+if __name__ == '__main__':
+    app.run(host='localhost',port=5000)
