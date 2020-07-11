d23b3b33448373cd7ab612c9755bb81aa811d86a
RyanHartzell <rah3156@rit.edu>
2020-07-11 12:37:20 -0400

    Made sure running app.py results in working app.

--- imagepypelines_tools/app.py
+++ imagepypelines_tools/app.py
@@ -14,27 +14,41 @@ Things I'm going to do here:
 from flask import Flask, flash, redirect, render_template, request, session, abort
 from flask_socketio import SocketIO, emit
 import os
-import networkx as nx
 
-app = Flask(__name__, template_folder='site', static_folder='site')
+# CHATROOM_ACTIVE = False
+
+app = Flask(__name__)
 app.debug = True
 socketio = SocketIO(app)
+
+# c = Chatroom(socketio) # this func needs to kick off Chatroom instance in it's own thread
+
+# start_chatroom(socketio)
 
 # cache_exists = os.path.exists(default_cache_dir)
 # default_cache_dir = os.path.join(os.path.expanduser('~'),'.imagepypelines')
 
-
+################################################################################
+# Basic Flask application funcs (html handling, rerouting, other basic web shit)
+################################################################################
 @app.route("/")
 def welcome():
-    return render_template("templates/sampleapplet.html")
+    return render_template("sampleapplet.html")
 
+################################################################################
+# Client generated event processors
+################################################################################
 @socketio.on('enter prog bar width')
 def progress(data):
     print("Here's our width:   ", data['width'])
     msg = {'width': data['width']}
     emit('return prog bar width', msg)
 
-
+################################################################################
+# Chatroom / Session / Pipeline generated event processors
+################################################################################
+def send_to_client(msg):
+    socketio.emit('pipeline-update', msg, broadcast=True)
 
 if __name__ == '__main__':
     socketio.run(app, host='localhost',port=5000)
