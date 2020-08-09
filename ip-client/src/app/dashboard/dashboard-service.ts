9300a833d53570ada3a617450f1e9798fced2092
Joe Bartelmo <joebartelmo@gmail.com>
2020-08-09 12:40:18 -0400

    progress

--- ip-client/src/app/dashboard/dashboard-service.ts
+++ ip-client/src/app/dashboard/dashboard-service.ts
@@ -5,9 +5,32 @@ import { of, Observable } from 'rxjs';
 import * as mockGraph from './mock/graph.json';
 import { IPWrapper } from '../models/IPWrapper';
 
+/**
+ * The Imagepypeline service establishes a websocket with
+ * this UI. The data is constantly emitted as it comes through
+ * the pypelines module. 
+ */
 @Injectable({providedIn: 'root'})
 export class DashboardService {
+
     private ipEventEmitter = new EventEmitter<IPWrapper>();
+    
+
+    /**
+     * Establishes websocket connection
+     */
+    public constructor() {
+
+    }
+
+    /**
+     * Subscribes to a websocket supplied by the commandline
+     * @todo add express service and retrieve commandline argument for location
+     */
+    public subscribeToWebsocket() {
+        const socket = new WebSocket();
+        socket.
+    }
 
     /**
      * Retrieves a graph by it's UUID from the pypeline service
